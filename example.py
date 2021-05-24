9931d0feb8399af07443cfcbcb1b3e32e59107a5
HassanAliRizvi <hasanijthedi2002@gmail.com>
2021-05-24 12:33:28 -0400

    example.py statement on new_branch

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
 def git_operation():
  print("I am adding example.py file to the remote repository. Making changes on 5/24/2021!")
+ print("Adding a statement to new_branch on 5/24/2021.")
 git_operation()
